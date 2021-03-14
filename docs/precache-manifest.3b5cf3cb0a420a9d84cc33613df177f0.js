37e533cc5b00db46f2680c7c387799b2f93a16d9
Roberto Villalobos <roberto.v.y@gmail.com>
2021-03-14 01:57:16 -0300

    index.html actualizado2

--- docs/precache-manifest.3b5cf3cb0a420a9d84cc33613df177f0.js
+++ docs/precache-manifest.3b5cf3cb0a420a9d84cc33613df177f0.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "30d7a50a6390321745c66ba99281342a",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "bb77535d96fbb36b9184",
-    "url": "/static/css/main.832776f2.chunk.css"
+    "url": "./static/css/main.832776f2.chunk.css"
   },
   {
     "revision": "8c639a6bff16b54b9f77",
-    "url": "/static/js/2.629c55af.chunk.js"
+    "url": "./static/js/2.629c55af.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.629c55af.chunk.js.LICENSE.txt"
   },
   {
     "revision": "bb77535d96fbb36b9184",
-    "url": "/static/js/main.457c2c68.chunk.js"
+    "url": "./static/js/main.457c2c68.chunk.js"
   },
   {
     "revision": "5bba4b9ed69c6568a70e",
-    "url": "/static/js/runtime-main.95118175.js"
+    "url": "./static/js/runtime-main.95118175.js"
   }
 ]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3b5cf3cb0a420a9d84cc33613df177f0.js"
+  "./precache-manifest.3b5cf3cb0a420a9d84cc33613df177f0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
